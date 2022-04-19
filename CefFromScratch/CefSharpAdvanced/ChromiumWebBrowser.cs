52d2583a8a1c9deb74a55eb5c8f1a7ed12c3067f
dmitriynaumov1024 <naumov1024@gmail.com>
2022-04-19 10:15:08 +0300

    + CefSharpAdvanced subproject
    Uses Mfetch to register async methods that return complex objects with minimal boilerplate but no modification to target object

---
+++ CefFromScratch/CefSharpAdvanced/ChromiumWebBrowser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp.WinForms;
+using Mfetch;
+
+namespace CefSharpAdvanced
+{
+    public class ChromiumWebBrowser: CefSharp.WinForms.ChromiumWebBrowser
+    {
+        MfetchObject mfetch;
+
+        public ChromiumWebBrowser(string address) : base(address) { }
+
+        public void RegisterMfetchObject(string name, object obj)
+        {
+            this.EnsureMfetchExists();
+            // this.mfetch.AddDelegate
+        }
+
+        public void RegisterMfetchDelegate<TOut>(string name, string subname, Func<TOut> f) where TOut: class
+        {
+            this.EnsureMfetchExists();
+            this.mfetch.AddDelegate(name+"."+subname, f);
+        }
+
+        public void RegisterMfetchDelegate<TIn, TOut>(string name, string subname, Func<TIn, TOut> f) where TOut: class
+        {
+            this.EnsureMfetchExists();
+            this.mfetch.AddDelegate(name+"."+subname, f);
+        }
+
+        private void EnsureMfetchExists()
+        {
+            if (this.mfetch == null)
+            {
+                this.mfetch = new MfetchObject();
+                this.RegisterAsyncJsObject("m", this.mfetch.SafeAdapter);
+            }
+        }
+    }
+}
